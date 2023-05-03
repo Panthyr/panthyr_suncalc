c866e47dd5dfd0d20534c7e38057059e6caf9d24
dietervansteenwegen <github@vansteenwegen.org>
2023-05-03 16:53:06 +0200

    prep for v1.0.0

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 
 setup(
     name='p_suncalc',
-    version='0.1.0-beta',
+    version='v1.0.0',
     packages=find_packages(),
     install_requires=[
         'pysolar @ git+ssh://git@github.com/pingswept/pysolar.git',
